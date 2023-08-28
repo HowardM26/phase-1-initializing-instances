9fd67875f163b83b8c454fa848417bf658d8e992
+ <+>
2023-08-27 20:20:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = "eggs";
+        this.drink = "juice";
+    }
+}
+
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad =  "side salad";
+        this.soup = "broccoli cheddar soup";
+        this.drink =  "iced tea";
+    }
+}
+
+class Dinner {
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        this.salad =  "balsamic salad";
+        this.soup = "consomme";
+        this.entree = "filet mignon"; 
+        this.#dessert = "cheesecake";
+    }
+
+}
+
